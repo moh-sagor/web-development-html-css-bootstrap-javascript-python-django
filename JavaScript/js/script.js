3f0f06fdd81bf7be9167ce79541020ba0a549c0c
Md Sagor Hossain <sagorhassain4@gmail.com>
2021-05-31 01:16:29 +0600

    31 may 2021

---
+++ JavaScript/js/script.js
@@ -0,0 +1,18 @@
+// console.log("Hi this is sagor ");
+// console.log("This is testing");
+// document.write("Js file is connected");
+
+// var x;
+// x = prompt("Enter value");
+// console.log(x);
+// document.write(x);
+
+// var Myvar , myvar;
+// Myvar = "asadfdf"
+// myvar = "qwerrt"
+
+// document.write(Myvar);
+// document.write(myvar);
+
+const a = 2;
+
