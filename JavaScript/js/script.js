858cab46b8cf6d01a70a02ef29920e5f31f6e542
Md Sagor Hossain <sagorhassain4@gmail.com>
2021-06-02 02:19:42 +0600

    2 june 2021

--- JavaScript/js/script.js
+++ JavaScript/js/script.js
@@ -14,5 +14,41 @@
 // document.write(Myvar);
 // document.write(myvar);
 
-const a = 2;
+// const a = 2;
+
+// Operator + - * / %
+
+// var a = 2;
+// var b = 3;
+// // var c = a + b;
+// // var c = a - b;
+// var c = a ** b;
+// // var c = a / b;
+// document.write(c)
+
+// increment
+// var a = 8;
+// a++;
+// // a--;
+// document.write(a);
+
+
+// var a = 5;
+// var b = 6;
+// var c,d ;
+// c = a++;
+// d = ++b;
+//  document.write(c);
+//  document.write(d);
+
+
+// var line1 = "Hello "
+// var line2 = "36"
+// document.write(line1+line2)
+
+
+// var C = prompt("Enter value");
+// var F = (9/5)*C+32;
+// alert("Fahrenhite: " +F+ " Degree");
+// console.log("Task Complete");
 
