52e0bf8cc4a79ce0760230eb95bd4dd6a8111d35
Md Sagor Hossain <sagorhassain4@gmail.com>
2021-06-06 01:19:39 +0600

    6 june 2021

--- JavaScript/js/script.js
+++ JavaScript/js/script.js
@@ -52,3 +52,72 @@
 // alert("Fahrenhite: " +F+ " Degree");
 // console.log("Task Complete");
 
+
+
+// ES6 feature
+// Template literals 
+// console.log(`First line
+// Second Line`)
+
+// `` = backtik
+// var name = "Rahim"
+// var age = 25;
+// var DOB = 1990
+// console.log(`His name is ${name}
+// His age is ${age}
+// His date of Birth ${DOB}`);
+
+// let a = 35;
+// let b = 28;
+// console.log(`${a} + ${b} = ${a+b}`);
+
+
+
+// ********control flow ************
+// if statement ?
+
+// var a;
+// a = prompt("Enter value of a");
+// var b;
+// b = prompt("Enter value of b");
+
+// if(a>b){
+//     console.log(`a = ${a} is Big`);
+//     document.write(`a = ${a} is Big`);
+// }
+// else if (a<b){
+//     console.log(`b = ${b} is Big`);
+//     document.write(`b = ${b} is Big`);
+// }
+// else{
+//     console.log(`a = ${a} and b = ${b} is equal `);
+//     document.write(`a = ${a} and b = ${b} is equal `);
+// }
+
+
+// Switch 
+
+// console.log(`Select a option :
+//  a. op-1 
+//  b. op-2 
+//  c. op-3`);
+// choice = prompt();
+// var text;
+// switch (choice) {
+//     case "a":
+//         text = "Option 1 selected";
+//         break;
+//     case "b":
+//         text = "Option 2 selected";
+//         break;
+//     case "c":
+//         text = "Option 3 selected";
+//         break;
+//     default:
+//         text = "no Option is selected";
+//         break;
+// }
+// document.write(text);
+
+
+
